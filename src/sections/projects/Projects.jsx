714fd864059d3f75639553ca7149ee0cc53ffa04
Umarbek1706 <rashidovumarbek1706@gmail.com>
2025-08-28 23:23:26 +0500

    fix eror

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -9,14 +9,14 @@ function Projects() {
 
       <div className={styles.grid}>
         <ProjectCard 
-         image="./images/Future_Study.png"
+         image="/images/Future_Study.png"
           title="Future-Study" 
           description="Online course"
           info="Discover our online learning platform, where you can easily register, complete interactive tests, and earn certificates to showcase your skills. Fully responsive and user-friendly, our website works seamlessly on computers, tablets, and smartphones, allowing you to learn anytime, anywhere." 
           link="https://future-study.vercel.app/" 
         />
         <ProjectCard 
-         image="/images/Eco_bazar.png"
+         image="/images/Eco_Bazar.png"
           title="Eco-bazar" 
           description="Online market" 
           info="Discover EcoBazar, your trusted online marketplace where you can easily explore eco-friendly products, shop with secure payments, and enjoy fast delivery. Fully responsive and user-friendly, our website works seamlessly on computers, tablets, and smartphones, making shopping simple and convenient anytime, anywhere." 
